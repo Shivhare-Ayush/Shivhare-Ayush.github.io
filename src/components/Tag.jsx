d42a656b33ea33718497d651a016897265cec4cd
Ayush-Shivhare <Ayush.Shivhare@ibm.com~>
2026-07-16 18:47:21 -0500

    feat: make Tag icon prop optional for text-only pills

--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -1,16 +1,16 @@
 //import React from 'react';
 import PropTypes from 'prop-types';
-// text-gray-800 for captions
+
 function Tag({ icon: Icon, title }) {
   return (
     <div className='flex items-center bg-transparent text-gray-100 backdrop-blur-sm border border-white/10 rounded-full px-3 py-1 m-1'>
-      <Icon className='text-xl' />
-      <span className='ml-2 font-[Anzo3]'>{title}</span>
+      {Icon && <Icon className='text-xl' />}
+      <span className={`font-[Anzo3]${Icon ? ' ml-2' : ''}`}>{title}</span>
     </div>
   );
 }
 Tag.propTypes = {
-    icon: PropTypes.elementType.isRequired,
-    title: PropTypes.string.isRequired,
+  icon: PropTypes.elementType,
+  title: PropTypes.string.isRequired,
 };
 export default Tag;
